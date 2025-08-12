1d02b9aea5e2e4dc27e8cba4b1fd84445e9707a9
NahyunLeylaLee <florence5107@gmail.com>
2025-08-11 23:26:06 -0700

    survey

--- src/survey/survey.service.ts
+++ src/survey/survey.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { PrismaClient } from 'prisma/generated/client';
 
 @Injectable()
-export class SurveyService {}
+export class SurveyService {
+  constructor(
+    @Inject('PRISMA_CLIENT') private readonly db: PrismaClient, //generated PrismaClient import!!
+  ) {}
+
+  getAll() {
+    return this.db.survey.findMany({
+      select: {
+        id: true,
+        createdAt: true,
+        title: true,
+        age: true,
+        term: true,
+        isActive: true,
+        IsMain: true,
+      }
+    });
+  }
+}
